d9bc477c20c6482ec3231bb01f4a9a4d6d43241f
zylstraa <zylstra.az@gmail.com>
2021-02-09 21:02:05 -0600

    2 SQL questions finished

--- data.sql
+++ data.sql
@@ -49,16 +49,64 @@ JOIN counties2
 USING (fipscounty)
 ORDER BY opioids_per_person DESC;
 
-
-
-
-
-
-
-
-
-
---Who are the top opioid prescibers for the state of Tennessee?
---What did the trend in overdose deaths due to opioids look like in Tennessee from 2015 to 2018?
 --Is there an association between rates of opioid prescriptions and overdose deaths by county?
+/*What I want...
+-overdose_deaths table (has fips county):
+	do this for the year 2017 since that's the year these prescription stats are for
+	already filtered on TN
+-fips_county table (to get county name)
+-*/
+
+
+WITH opioids AS (SELECT drug_name
+FROM drug
+WHERE opioid_drug_flag = 'Y'),
+--don't need generic name as it returns same number of rows.
+
+TNdocs AS (SELECT npi, nppes_provider_zip5
+FROM prescriber
+WHERE nppes_provider_state = 'TN'),
+
+opioids_prescribed AS (SELECT npi, SUM(total_claim_count) AS number_prescribed
+FROM prescription
+JOIN opioids
+USING (drug_name)
+GROUP BY npi),
+
+opioids_TNdocs AS (SELECT number_prescribed, nppes_provider_zip5
+FROM opioids_prescribed
+JOIN TNdocs
+USING (npi)),
+
+counties AS (SELECT zip, fipscounty, SUM(number_prescribed) AS number_prescribed
+FROM zip_fips AS z
+JOIN opioids_TNdocs AS o
+ON z.zip = o.nppes_provider_zip5
+GROUP BY zip, fipscounty),
+
+counties2 AS (SELECT county, fipscounty, number_prescribed
+FROM fips_county
+JOIN counties
+USING (fipscounty)),
+
+overdoses AS (SELECT SUM(overdose_deaths) AS deaths, fipscounty
+FROM overdose_deaths
+WHERE year = 2017
+GROUP BY fipscounty),
+
+overdoses_c AS (SELECT county, deaths
+FROM fips_county
+JOIN overdoses
+USING (fipscounty))
+
+SELECT SUM(number_prescribed) AS num_prescribed, county, deaths
+FROM overdoses_c
+JOIN counties2
+USING (county)
+GROUP BY county, deaths
+ORDER BY deaths DESC
+
+
+
+
 --Is there any association between a particular type of opioid and number of overdose deaths?
